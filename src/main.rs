8e6cebf2a8241cb33d5904bf6d92f12ab15e4244
asmcos <asmcos@gmail.com>
2022-03-18 21:40:17 +0800

    use libpcap

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,28 @@ mod lib;
 
 use crate::lib::parse;
 use libpcap;
+use std::slice;
 
 fn main (){
 
     println!("{}","Welcome to use nets");
 
-	let devs = libpcap::lookup();	
+	let dev = libpcap::lookup();	
 
-    println!("{}",devs);
+    println!("{}",dev);
+
+    let mut Packet = libpcap::open(dev.as_str());
+
+
+    while let data = libpcap::next_ex(&mut Packet){
+        println!("Packet Length {:?}",Packet.head.len);
+
+        let parse = parse::PacketParse::new();
+        let data = unsafe { slice::from_raw_parts(Packet.data, Packet.head.len.try_into().unwrap()) };
+        let eth = parse.parse_link_layer(data);
+        println!("{:?}",eth);
+    }
+
+    libpcap::close(&mut Packet); 
 	
 }
